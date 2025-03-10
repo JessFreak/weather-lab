9a57f560575969a4a1b3085199cef9edd17170ad
JessFreak <maliy.olexander@lll.kpi.ua>
2025-03-10 11:39:52 +0200

    express init

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express');
+});
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log('Server is running on http://localhost:3000');
+});
