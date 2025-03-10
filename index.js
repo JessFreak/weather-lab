3e9d01fe19e853617e3124e7eae74c8372299341
JessFreak <maliy.olexander@lll.kpi.ua>
2025-03-10 13:04:46 +0200

    added base templates

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ import express from 'express';
 import 'dotenv/config';
 
 const app = express();
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express');
+app.get('/weather', (req, res) => {
+  res.render('weather');
+});
+
+app.get('/weather/:city', (req, res) => {
+  const city = req.params.city;
+  res.render('city', { city });
 });
 
 const port = process.env.PORT;
